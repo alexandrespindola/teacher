9d968986c80d96b9ffa2cb6c85cbc1d5561af073
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-04-05 17:34:20 +0100

    Atualizações no formulário de cadastro de professores e estilos CSS

--- src/components/TeacherForm.vue
+++ src/components/TeacherForm.vue
@@ -4,20 +4,51 @@
             <section class="flex flex-col gap-5">
                 <h3>Include teacher</h3>
                 <div class="flex flex-col items-center gap-5">
-                    <div class="flex flex-row gap-3 items-center"><label>First name:</label><input type="text"></div>
-                    <div class="flex flex-row gap-3 items-center"><label>Last name:</label><input type="text"></div>
-                    <div class="flex flex-row gap-3 items-center"><label>DNI/NIF:</label><input type="text"></div>
-                    <div class="flex flex-row gap-3 items-center"><label>Subjects</label><input type="text"><button
-                            class="button">Include</button></div>
+                    <div class="form__items"><label>First name:</label><input type="text" v-model="teacher.firstName">
+                    </div>
+                    <div class="form__items"><label>Last name:</label><input type="text" v-model="teacher.lastName">
+                    </div>
+                    <div class="form__items"><label>DNI/NIF:</label><input type="text" v-model="teacher.dni"></div>
+                    <div class="form__items"><label>Subjects</label><input type="text" v-model="subject"><button
+                            class="button" @click="handleSubject()">Include</button></div>
+                    <div>
+                        <ul>
+                            <li v-for="(element, index) in teacher.subjects" :key="index">{{ element }}</li>
+                        </ul>
+                    </div>
                     <div class="flex flex-row gap-3">
-                        <input type="checkbox" />
+                        <input type="checkbox" v-model="teacher.documentation" />
                         <p>Documentation provided</p>
                     </div>
-                    <button class="button">Include teacher</button>
+                    <button class="button" @click="handleTeacher()">Include teacher</button>
                 </div>
             </section>
             <section class="flex flex-col gap-5">
                 <h3>Teachers List</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>DNI/NIF</th>
+                            <th>Subjects</th>
+                            <th>Documentation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(element) in teachers" :key="element.dni">
+                            <td>{{ element.firstName }}</td>
+                            <td>{{ element.lastName }}</td>
+                            <td>{{ element.dni }}</td>
+                            <td>
+                                <ul>
+                                    <li v-for="(subject, index) in element.subjects" :key="index">{{ subject }}</li>
+                                </ul>
+                            </td>
+                            <td>{{ element.documentation ? 'Yes' : 'No' }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </section>
         </div>
     </div>
@@ -34,18 +65,50 @@ interface ITeacher {
     documentation: boolean
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-let teacher: Ref<ITeacher> = ref({
+const teacher: Ref<ITeacher> = ref({
     firstName: '',
     lastName: '',
     dni: '',
     subjects: [],
     documentation: false
 })
+
+const teachers: Ref<Array<ITeacher>> = ref([])
+
+const subject: Ref<string> = ref('')
+
+const handleSubject = () => {
+    teacher.value.subjects.push(subject.value)
+    subject.value = ''
+}
+
+
+const handleTeacher = () => {
+    teachers.value.push(teacher.value)
+    teacher.value = {
+        firstName: '',
+        lastName: '',
+        dni: '',
+        subjects: [],
+        documentation: false
+    }
+}
 </script>
 
 <style scoped lang="scss">
 h3 {
     @apply text-center text-2xl font-bold;
+}
+
+.button {
+    @apply bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700;
+}
+
+.form__items {
+    @apply flex flex-row gap-3 items-center
+}
+
+td {
+    @apply text-center bg-slate-100
 }
 </style>
